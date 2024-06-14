560f3955f9ce5ba9b13f917c5c4125d1ad34cad6
Yaroslava <yaroslava.hovenko@gmail.com>
2024-06-15 00:02:57 +0300

    init back

---
+++ src/constans/movies-constants.js
@@ -0,0 +1,1 @@
+export const releaseYearRegexp = /^\d{4}$/; // регулярний вираз року (який складається з 4 цифр ) (new Regexp(/^\d{4}$/))
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import { releaseYearRegexp } from './constans/movies-constants.js';
+
+const validationResult = releaseYearRegexp.test('200');
+console.log(validationResult); // це як валідатор - повертає тру/ фолс
