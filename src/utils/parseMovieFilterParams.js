a54c88bd292e30d2212dc10f7e354997b1ce8cc0
Yaroslava <yaroslava.hovenko@gmail.com>
2024-07-18 17:49:28 +0300

    filter favourite working with bags

--- src/utils/parseMovieFilterParams.js
+++ src/utils/parseMovieFilterParams.js
@@ -1,31 +1,31 @@
 //фільтр створюємо для кожної колекції окремо
 
-// import { typeList } from '../constans/movies-constants.js';
+import { typeList } from '../constans/movies-constants.js';
 
 //перетворюємо на буль
 const parsedBoolean = (value) => {
-  //   if (typeof value !== 'string') return;
+  if (typeof value !== 'string') return;
 
-  //   if (!['true', 'false'].includes(value)) return;
+  if (!['true', 'false'].includes(value)) return;
 
-  //   const parsedValue = JSON.parse(value);
+  const parsedValue = JSON.parse(value);
 
-  //   return parsedValue;
+  return parsedValue;
   ///////
-  if (value === 'true') return true;
-  if (value === 'false') return false;
-  return undefined;
+  //   if (value === 'true') return true;
+  //   if (value === 'false') return false;
+  //   return undefined;
 };
 
 const parseMovieFilterParams = ({ type, favourite }) => {
   // витягуємо тип з квері
 
-  //   const parsedType = typeList.includes(type) ? type : null;
+  const parsedType = typeList.includes(type) ? type : null;
 
   const parsedFavourite = parsedBoolean(favourite);
 
   return {
-    type,
+    type: parsedType,
     favourite: parsedFavourite,
   };
 };
